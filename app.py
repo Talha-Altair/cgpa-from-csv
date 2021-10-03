5b157ca5b30b1251fb16917485a863ccf32ebba2
Talha-Altair <cooltalha27@gmail.com>
2021-10-03 11:58:39 +0530

    Updates for 2021-10-03 11:58:39

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv('grades.csv').fillna(0)
+df = pd.read_csv('talha.csv').fillna(0)
 
 num_of_sems = int(len(df.columns)/2)
 
@@ -16,17 +16,29 @@ cred_cols = [col for col in cols if 'cred' in col]
 
 for i in range(num_of_sems):
 
+    current_numerator = []
+
+    current_cred_list = []
+
     for grade,cred in zip(df[grade_cols[i]],df[cred_cols[i]]):
 
         current_offset = grade * cred
 
-        numerator_list.append(current_offset)
+        current_numerator.append(current_offset)
 
-        cred_list.append(cred)
+        current_cred_list.append(cred)
+
+    numerator_list.extend(current_numerator)
+
+    cred_list.extend(current_cred_list)
+
+    current_gpa = sum(current_numerator)/sum(current_cred_list)
+
+    print(f"Sem {i+1} GPA : {current_gpa}")
 
 cgpa = sum(numerator_list)/sum(cred_list)
 
-print(cgpa)
+print(f"cgpa:{cgpa}")
 
 
 
